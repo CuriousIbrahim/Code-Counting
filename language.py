8e886aff5b3489edcb939d321cc0f1173cab11e2
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-06-27 11:16:36 -0400

    Add method that reads all languages from languages.txt and returns list of them

--- language.py
+++ language.py
@@ -16,6 +16,40 @@ class Language:
         return 'NAME: {} EXTENSION: {}'.format(self.name, self.extension)
 
 
+LANGUAGES_FILE = 'languages.txt'
+
+def get_languages():
+
+    def parse(data):
+        # Split between ';', you would have a list with length 2, index 0 is name while index 1 is extension
+        data = data.split(';')
+
+        # Clean name (remove \n)
+        data[0].rstrip('\n')
+
+        # There might be multiple extensions for a single language, so split at ','
+        data[1] = data[1].split(',')
+
+        # Clean extensions (remove \n)
+        for index, line in enumerate(data[1]):
+            data[1][index] = line.strip()
+
+        return data
+
+    languages = []
+
+    with open(LANGUAGES_FILE) as f:
+
+        # Skip header row
+        next(f)
+
+        for line in f:
+            data = parse(line)
+
+            # Create new Language object
+            languages.append(Language(data[0], data[1]))
+
+    return languages
 
 
 
