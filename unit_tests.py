61139c993f83307c81ed9730d9099ace08a5f8c8
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-07-15 22:13:03 -0400

    Add get_line_count method to remove the repetition in the test_{lang}_line_count methods

--- unit_tests.py
+++ unit_tests.py
@@ -4,25 +4,27 @@ from lang_controller import LanguagesController
 
 CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
 
-def test_java_line_count():
+TEST_CODE_DIR = 'test-code/'
+
+JAVA_FILE = os.path.join(TEST_CODE_DIR, 'Person.java')
+PYTHON_FILE = os.path.join(TEST_CODE_DIR, 'Person.py')
+
+def get_line_count(file, language_extension):
     lang_controller = LanguagesController()
 
-    path = os.path.join(CURRENT_PATH, 'test-code/Person.java')
+    path = os.path.join(CURRENT_PATH, file)
 
     lang_controller.check(path)
 
-    result = lang_controller.get_used_language('.java').lines_of_code
+    result = lang_controller.get_used_language(language_extension).lines_of_code
 
-    assert result == 39
+    return result
+
+def test_java_line_count():
+
+    assert get_line_count(JAVA_FILE, '.java') == 39
 
 
 def test_python_line_count():
-    lang_controller = LanguagesController()
 
-    path = os.path.join(CURRENT_PATH, 'test-code/Person.py')
-
-    lang_controller.check(path)
-
-    result = lang_controller.get_used_language('.py').lines_of_code
-
-    assert result == 16
+    assert get_line_count(PYTHON_FILE, '.py') == 16
