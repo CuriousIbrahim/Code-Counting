586b9686a6523134e93a9c4404fe094cd88f7503
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-07-15 22:38:09 -0400

    Move explore_folder method to util.py

--- code-counting.py
+++ code-counting.py
@@ -1,24 +1,9 @@
-import os
 import time
 import sys
-from lang_controller import LanguagesController
 import logging
 
-files = []
-
-
-def explore_folder(root):
-
-    if not os.path.isdir(root):
-        logging.info('\tAdd', root, 'to files list')
-        files.append(root)
-
-    if os.path.isdir(root):
-
-        for f in os.listdir(root):
-            explore_folder(os.path.join(root, f))
-
-    return files
+from lang_controller import LanguagesController
+from util import explore_folder
 
 
 def main(root):
@@ -29,7 +14,7 @@ def main(root):
     start = time.time()
 
     logging.info('Exploring', root, ':')
-    explore_folder(root)
+    files = explore_folder(root, [])
     logging.info('Exploring finished')
 
     for f in files:
--- util.py
+++ util.py
@@ -1,3 +1,5 @@
+import logging
+import os
 
 
 def get_file_extension(file):
@@ -16,3 +18,16 @@ def count_lines_of_code(file):
             count += 1
 
     return count
+
+
+def explore_folder(root, files):
+
+    if not os.path.isdir(root):
+        logging.info('\tAdd', root, 'to files list')
+        files.append(root)
+
+    if os.path.isdir(root):
+        for f in os.listdir(root):
+            explore_folder(os.path.join(root, f), files)
+
+    return files
