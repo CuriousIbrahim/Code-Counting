3056456594cd89a22ddf2dc2d327020b2f3d4e02
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-07-16 05:49:50 -0400

    Add 2 more tests for when a file does not exist

--- tests/file_does_not_exist_test.py
+++ tests/file_does_not_exist_test.py
@@ -15,3 +15,23 @@ def test_a_file_that_does_not_exist():
     result = lang_controller.get_used_language('java')
 
     assert result is None
+
+
+def test_a_file_does_not_exist_line_count():
+    lang_controller = LanguagesController()
+
+    path = os.path.join(CURRENT_PATH, 'does_not_exist.java')
+
+    lang_controller.check(path)
+
+    assert lang_controller.total_lines() == 0
+
+
+def test_a_file_does_not_exist_file_count():
+    lang_controller = LanguagesController()
+
+    path = os.path.join(CURRENT_PATH, 'does_not_exist.java')
+
+    lang_controller.check(path)
+
+    assert lang_controller.total_files() == 0
