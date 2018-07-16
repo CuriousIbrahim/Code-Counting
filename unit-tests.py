f5c4e9e7f9cfd8f2df7a55c1597e68e2e87698fb
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2018-07-15 22:43:32 -0400

    Add test that checks total line count of test-code/ dir

--- unit-tests.py
+++ unit-tests.py
@@ -1,6 +1,7 @@
 import pytest
 import os
 from lang_controller import LanguagesController
+from util import explore_folder
 
 CURRENT_PATH = os.path.dirname(os.path.realpath(__file__))
 
@@ -10,6 +11,13 @@ JAVA_FILE = os.path.join(TEST_CODE_DIR, 'Person.java')
 PYTHON_FILE = os.path.join(TEST_CODE_DIR, 'Person.py')
 HTML_FILE = os.path.join(TEST_CODE_DIR, 'test.html')
 CSS_FILE = os.path.join(TEST_CODE_DIR, 'test.css')
+
+JAVA_LINE_COUNT = 39
+PYTHON_LINE_COUNT = 16
+HTML_LINE_COUNT = 29
+CSS_LINE_COUNT = 9
+
+TEST_CODE_DIR_LINE_COUNT = JAVA_LINE_COUNT + PYTHON_LINE_COUNT + HTML_LINE_COUNT + CSS_LINE_COUNT
 
 
 def get_line_count(file, language_extension):
@@ -26,21 +34,37 @@ def get_line_count(file, language_extension):
 
 def test_java_line_count():
 
-    assert get_line_count(JAVA_FILE, '.java') == 39
+    assert get_line_count(JAVA_FILE, '.java') == JAVA_LINE_COUNT
 
 
 def test_python_line_count():
 
-    assert get_line_count(PYTHON_FILE, '.py') == 16
+    assert get_line_count(PYTHON_FILE, '.py') == PYTHON_LINE_COUNT
 
 
 def test_html_line_count():
 
-    assert get_line_count(HTML_FILE, '.html') == 29
+    assert get_line_count(HTML_FILE, '.html') == HTML_LINE_COUNT
 
 
 def test_css_line_count():
 
-    assert get_line_count(CSS_FILE, '.css') == 9
+    assert get_line_count(CSS_FILE, '.css') == CSS_LINE_COUNT
+
+
+# Get line count for the entire test-code/ dir
+def test_the_test_code_dir_line_count():
+
+    lang_controller = LanguagesController()
+
+    files = explore_folder(TEST_CODE_DIR, [])
+
+    for file in files:
+        lang_controller.check(file)
+
+    assert lang_controller.total_lines() == TEST_CODE_DIR_LINE_COUNT
+
+
+
 
 
