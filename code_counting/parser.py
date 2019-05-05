c1355b81e0a7105276e783bf7a37ea02b1c5fd9f
Ibrahim Abdul Hameed <ibrahim74115@gmail.com>
2019-05-05 02:01:21 -0400

    Parse directory if only argument given

--- code_counting/parser.py
+++ code_counting/parser.py
@@ -13,7 +13,7 @@ def parse(argv):
 
     buffer = []
 
-    for v in argv:
+    for i, v in enumerate(argv):
 
         if '-d' == v:
             temp = ''.join(buffer)
@@ -30,6 +30,10 @@ def parse(argv):
 
         else:
             buffer.append(v)
+
+        if i == len(argv) - 1:
+            temp = ''.join(buffer)
+            result['dir'] = temp
 
     return result
 
